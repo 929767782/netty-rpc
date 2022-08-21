d5ba84240fac991402273778dbae5a149e2c6634
llp <929767782@qq.com>
2022-08-21 22:40:53 +0800

    新增服务监听功能

--- netty-rpc-client/src/main/java/com/llp/rpc/client/discovery/ZkServiceDiscovery.java
+++ netty-rpc-client/src/main/java/com/llp/rpc/client/discovery/ZkServiceDiscovery.java
@@ -7,11 +7,13 @@ import com.llp.rpc.common.loadBalancer.LoadBalancer;
 import com.llp.rpc.common.loadBalancer.RoundRobinRule;
 import com.llp.rpc.common.zookeeper.CuratorClient;
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * zookeeper实现 服务发现功能
@@ -23,6 +25,8 @@ public class ZkServiceDiscovery implements ServiceDiscovery{
     private CuratorClient curatorClient;
 
     private final LoadBalancer loadBalancer;
+
+    private List<InetSocketAddress> socketAddressesCache;
 
 
     public ZkServiceDiscovery(String registryAddress, LoadBalancer loadBalancer) {
@@ -40,24 +44,28 @@ public class ZkServiceDiscovery implements ServiceDiscovery{
     @Override
     public InetSocketAddress getService(String serviceName){
         try {
-            byte[] data = curatorClient.getData(Config.getZookeeperRegistryPath()+"/"+serviceName);
-//            curatorClient.watchNode(Config.getZookeeperRegistryPath() + "/" + serviceName, new Watcher() {
-//                @Override
-//                public void process(WatchedEvent watchedEvent) {
-//
-//                }
-//            });
-            String allAddress = new String(data);
-            List<InetSocketAddress> socketAddresses = new ArrayList<>();
-            for(String address : allAddress.split("#")){
-                String[] hostAndPort = address.split(":");
-                InetSocketAddress socketAddress = new InetSocketAddress(hostAndPort[0],Integer.parseInt(hostAndPort[1]));
-                socketAddresses.add(socketAddress);
+            if(socketAddressesCache==null || socketAddressesCache.size()==0){
+                byte[] data = curatorClient.getData(Config.getZookeeperRegistryPath()+"/"+serviceName);
+                curatorClient.watchNode(Config.getZookeeperRegistryPath() + "/" + serviceName, new Watcher() {
+                    @Override
+                    public void process(WatchedEvent watchedEvent) {
+                        String path = watchedEvent.getPath();
+                        updateService(path);
+                    }
+                });
+                String allAddress = new String(data);
+                socketAddressesCache = new CopyOnWriteArrayList<>();
+                for(String address : allAddress.split("#")){
+                    String[] hostAndPort = address.split(":");
+                    InetSocketAddress socketAddress = new InetSocketAddress(hostAndPort[0],Integer.parseInt(hostAndPort[1]));
+                    socketAddressesCache.add(socketAddress);
+                }
             }
+
             if(loadBalancer instanceof ConsistentHashRule){
-                return loadBalancer.getInstance(socketAddresses, InetAddress.getLocalHost().getHostAddress());
+                return loadBalancer.getInstance(socketAddressesCache, InetAddress.getLocalHost().getHostAddress());
             }else{
-                return loadBalancer.getInstance(socketAddresses);
+                return loadBalancer.getInstance(socketAddressesCache);
             }
         } catch (KeeperException.NoNodeException e){
             throw new RuntimeException("找不到对应服务");
@@ -65,5 +73,29 @@ public class ZkServiceDiscovery implements ServiceDiscovery{
             e.printStackTrace();
         }
         return null;
+    }
+
+    private void updateService(String path){
+        try {
+            byte[] data = curatorClient.getData(path);
+            curatorClient.watchNode(path, new Watcher() {
+                @Override
+                public void process(WatchedEvent watchedEvent) {
+                    String path = watchedEvent.getPath();
+                    updateService(path);
+                }
+            });
+            String allAddress = new String(data);
+            socketAddressesCache = new CopyOnWriteArrayList<>();
+            for(String address : allAddress.split("#")){
+                String[] hostAndPort = address.split(":");
+                InetSocketAddress socketAddress = new InetSocketAddress(hostAndPort[0],Integer.parseInt(hostAndPort[1]));
+                socketAddressesCache.add(socketAddress);
+            }
+        } catch (KeeperException.NoNodeException e){
+            throw new RuntimeException("找不到对应服务");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
